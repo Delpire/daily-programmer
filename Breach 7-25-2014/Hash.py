847b59213f8d4a65af129e4bb3f115bbb3547fdb
Chris Delpire <cadelpire@bellsouth.net>
2014-08-06 22:48:31 -0500

    Fixed message length, divided chunks
    Binary is now divided up into chunks. Each chunk will be broken into 16
    strings of bits that will be manipulated.

--- Breach 7-25-2014/Hash.py
+++ Breach 7-25-2014/Hash.py
@@ -1,40 +1,66 @@
-#Return original message padded up to the next multiple of 512.
-def pad(result, mLength):
+#Returns an array of a binary converted from a string.
+def convertToBin(string):
 
-    multiple = int(size/512)
+    result = []
+
+    for char in string:
+        #Converts the character into a bit.
+        bits = bin(ord(char))[2:]
+        #Adds zeros to make the number 8 bits long.
+        bits = '00000000'[len(bits):] + bits
+        #Adds the bits to the array.
+        result.extend([int(bit) for bit in bits])
+
+    return result
+
+
+#Return original message padded up to the next multiple of 512.
+def pad(result, messageLength):
+
+    multiple = int(binaryLength/512)
 
     #Add a 1 to the end.
     if len(result) < (512 * multiple + 448):
         result.extend([1])
 
     #Add 0's up to the size of the original message in bits under the next multiple of 512.
-    result.extend([0 for i in range(len(result), 512 * (multiple + 1) - len(mLength))])
+    result.extend([0 for i in range(len(result), 512 * (multiple + 1) - len(messageLength))])
           
-    result.extend([int(bit) for bit in mLength])
+    result.extend([int(bit) for bit in messageLength])
 
     return result
 
-plaintext = "Hicccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc";
+plaintext = "A Test";
 
-result = []
+#Convert the plaintext into binary.
+result = convertToBin(plaintext)
 
-messageLength = len(plaintext)sss
+#Get the length of the message converted to binary.
+binaryLength = len(result)
 
-#Convert the plaintext into binary and store it in an array.
-for char in plaintext:
-    #Converts the character into a bit.
-    bits = bin(ord(char))[2:]
-    #Adds zeros to make the number 8 bits long.
-    bits = '00000000'[len(bits):] + bits
-    #Adds the bits to the array.
-    result.extend([int(bit) for bit in bits])
+#If the length is not a multiple of 512 then pad the message.
+if binaryLength % 512 != 0:
+    result = pad(result, bin(binaryLength)[2:])
 
-#Get the size of the message converted to binary.
-size = len(result)
+multiples = int(len(result) / 512)
 
-#If the size is not a multiple of 512 then pad the message.
-if size % 512 != 0:
-    result = pad(result, bin(messageLength)[2:])
+print(multiples)
+print(result[0:512])
+words = [[] for y in range(80)]
+
+#Break the binary up into 512 blocks.
+for i in range(0, multiples):
+
+    #Break each block up into sixteen 32-bit words.
+    for j in range(0, 16):
+        words[j] = result[i*512 + j*32:(i*512 + j*32) + 32]
+        print(words[j])
+
+    for j in range(16,80):
+        words[j-3]
+        words[j-8]
+        words[j-14]
+        words[j-16]
 
 print(len(result))
 
